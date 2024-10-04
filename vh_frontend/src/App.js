5f3e1b7a142c7992b11b8c3b392d0d0ce93b0dcb
Meet7773 <mp210954@gmail.com>
2024-10-05 01:33:53 +0530

    first

--- vh_frontend/src/App.js
+++ vh_frontend/src/App.js
@@ -3,6 +3,7 @@ import FirstPage from './dashboard/First';
 import Stats from './Stats/Stats1'
 import Profile from './profile/profile';
 import Redeem from './redeem/Redeem';
+import Team from './Team/Team';
 
 
 import {
@@ -20,6 +21,7 @@ const App = () => (
   <Route exact path="stats" element={<Stats />} />
   <Route exact path="profile" element={<Profile />} />
   <Route exact path="redeem" element={<Redeem />} />
+  <Route exact path="Team" element={<Team />} />
 
   
  
